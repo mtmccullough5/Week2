410f6a214e7a8c0d986fc383162be20e377a78a2
Matthew McCullough <mcculloughmatthew5@gmail.com>
2017-08-23 21:16:29 -0600

    Anew day

---
+++ Day2-3.js
@@ -0,0 +1,199 @@
+//console.log("Hello")
+// Single Line Comment
+/* Multi line comment */
+
+/* Datatypes 
+Boolean: true/false
+String: 'some string'
+Number:
+  Integer: 1
+  Float: 2.545
+null: null
+undefined: 'undefined'
+Date:
+Array: [1,2,3,4,4]
+Object: {a: 1, b:2}
+*/
+
+//console.log(1+1)
+
+// Comparators
+/*
+4 >  2
+4 >= 2
+
+X == 3
+x !=3
+
+// Add third equal sign so that type is checked
+
+console.log( 2 === '2')
+
+*/
+// And operator (logical operator)
+// x > 5 && X < 9
+// Or (logical operator)
+// x === 5 || x ===3
+
+
+//var name = 'Matt'
+// Camel case is the default 
+//var myNameIs
+// cant start with a number
+/*
+var balance = 1000;
+console.log(balance)
+
+balance++
+console.log(balance)
+balance--
+*/
+
+/*
+var firstName = "John";
+console.log("Hello " + firstName)
+console.log("Ready or not \n here is a new line")
+*/
+
+// properties don't have paranthesis
+// methods always have to have paranthesis
+/*
+var number = Number("1.9903")
+number = parseFloat("1.65563")
+//.toFixed()
+//console.log(number.toPrecision(2))
+//Math.random()
+console.log( Math.floor(Math.random()*6+1))
+Math.round(8.4)
+console.log(Math.PI)
+*/
+
+// Loops
+
+/*
+for( var i = 0; i < 10; i++) {
+  console.log(i);
+
+}
+
+
+var i = 0;
+do {
+  console.log(i++)
+} while ( i < 5)
+
+if (x === 5) {
+  console.log()
+} else if {
+  
+}
+
+var operator = '+';
+switch() {
+  case '':
+    "something"
+    break;
+  default:
+}
+*/
+/* function 
+console.log(sayHello(3,2))
+
+function sayHello(num1,num2) {
+  return num1+num2
+  console.log("hello");
+}
+console.log(sayHello(1,2))
+*/
+/* Call these loops on an array
+forEach
+map
+reduce
+filter
+*/
+
+// for each loop
+/*
+var numbers = [1,4,9];
+var sum = 0;
+
+numbers.forEach(function(num) {
+  sum += num
+})
+console.log(sum)
+
+function addColor(url, color, cb) {
+  setTimeout(function(){return cb();},1);
+}
+
+for (var i in colors) {
+  addColor()
+}
+*/
+/*
+var nums = [1,2,3,4]
+var squared = nums.map( function(number) {
+  return Math.pow(number, 2);
+})
+
+console.log(squared);
+console.log(nums);
+*/
+
+// var sum = nums.reduce()
+/*
+var person = { name: "John", age: 36};
+person.age = 40;
+
+var pelop = function(firstName,lastName) {
+  
+}
+*/
+
+var message = document.getElementById('myInput').target.value
+
+
+var btn = document.getElementById("btn")
+btn.addEventListener('click', function(message) {
+  alert(message)
+})
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
